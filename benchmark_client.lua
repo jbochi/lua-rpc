55972b84cd1b6468fbbaabf530d0750895a74932
Juarez Bochi <jbochi@gmail.com>
2014-04-06 11:45:36 -0300

    Size benchmark

--- benchmark_client.lua
+++ benchmark_client.lua
@@ -8,20 +8,21 @@ local clients = {
 }
 
 local tests = {}
-
 tests.correction = function(client)
   assert(client.foo(1, 2, 3) == 3)
   assert(client.foo2() == nil)
   assert(client.boo("teste") == 5)
 end
+tests.correction.times = 100
 
 for test_name, test in pairs(tests) do
   for client_name, client in pairs(clients) do
+    times = test.times
     start_time = os.clock()
     test(client)
     end_time = os.clock()
 
     elapsed_time = end_time - start_time
-    print(test_name, client_name, string.format("%.5fs", elapsed_time))
+    print(test_name, client_name, times, string.format("%.5fs", elapsed_time))
   end
 end
